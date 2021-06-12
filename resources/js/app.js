8af300627925b5d284a9919179b3897e597889fe
Maswebber <mas.webber01@gmail.com>
2021-06-12 20:13:35 +0700

    Remove header

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,15 +16,8 @@ const axios = require('axios');
             q:textto.value,
         };
         const cross={crossDomain:true}
-        
-        const header={
-          header:{
-          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-          'User-Agent': 'AndroidTranslate/5.3.0.RC02.130475354-53000263 5.1 phone TRANSLATE_OPM5_TEST_1',
-          }
-        }
         // console.log(data)
-        axios.post(url,data,header,cross)
+        axios.post(url,data,cross)
           .then(function (response) {
             textend.value=response.data.sentences;
           })
