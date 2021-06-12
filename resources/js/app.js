1a984d11c5d3256f2d38345e6747675d75e4a8dd
Maswebber <mas.webber01@gmail.com>
2021-06-12 16:19:59 +0700

    Add AdonisJS

---
+++ resources/js/app.js
@@ -0,0 +1,46 @@
+import '../css/app.css'
+const axios = require('axios');
+
+(function(){
+    var sl=document.getElementById('sl');
+    var tl=document.getElementById('tl');
+    var textto=document.getElementById('textto');
+    var textend=document.getElementById('textend');
+    var translate=document.getElementById('formTrans');
+    translate.addEventListener('submit',function(e){
+        e.preventDefault();
+        const url = `https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&hl=${tl.value}&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&iid=1dd3b944-fa62-4b55-b330-74909a99969e`;
+        const data={
+            sl:sl.value,
+            tl:tl.value,
+            q:textto.value,
+        };
+        // const header={
+        //     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        //     'User-Agent': 'AndroidTranslate/5.3.0.RC02.130475354-53000263 5.1 phone TRANSLATE_OPM5_TEST_1',
+        // }
+        // console.log(data)
+        axios.post(url,data)
+          .then(function (response) {
+            console.log(response.data);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    });
+})();
+
+//   const data = {
+//     'sl': sourceLang,
+//     'tl': targetLang,
+//     'q': text,
+//   };
+
+// return request({
+// method: 'POST',
+// uri: url,
+// encoding: 'UTF-8',
+
+// form: data,
+// json: true,
+// })
